06d4b7006452f63e97bb869be9e6634bac1bfc31
Dennis Kupec <denniskupec@gmail.com>
2013-06-05 06:24:26 -0400

    Added more errors. Key checking rough draft done. (Not Tested)

--- check_licence.php
+++ check_licence.php
@@ -3,6 +3,7 @@
 /* 
 *
 * Easy License
+* Licence Checker
 * (c)2013 Dennis Kupec
 *
 */
@@ -30,10 +31,18 @@ define("er130", "The key provided is invalid."); // Invalid product key
 define("er140", "The key provided is valid."); // Product key is valid
 define("er150", "Define a valid owner ID. (GET 'a')"); // Owner ID GET not recieved
 define("er160", "Define a product key. (GET 'b')"); // Product key GET not recieved
+define("er170", "MySQL error. Check configuration."); // MySQL Error.
+define("er180", "Owner-ID does not exist."); // Owner does not exist
+
+define("er999", "Product key is valid.");
+define("er998", "Product key has expired or is invalid.");
 
 // ===================================================
 // End Configuration
 // ===================================================
+
+// Global time variable
+$time = time();
 
 // You can disable queries for maintenance or w/e
 if(ALLOW == FALSE) die(er100);
@@ -42,20 +51,51 @@ if(ALLOW == FALSE) die(er100);
 $ownerid = $_GET['a'];
 $productkey = $_GET['b'];
 
-//Check if the GET data is valid
+// Check if the GET data is valid
 if(!isset($ownerid) or empty($ownerid)) die(er150);
 if(!isset($productkey) or empty($productkey)) die(er160);
 
+// Initialize MySQL
+try {  
+	$dbc = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);  
+	$dbc->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+}  
+catch(PDOException $e) {    
+	file_put_contents('sql_errorfile.txt', $e->getMessage(), FILE_APPEND); //make sure to delete this file after fixing problems
+	die(er170);
+} 
 
+// Start querying databases
+// Check for owner existence
 
+$sta = $dbc->prepare("SELECT * FROM `owner-list` WHERE owner-id=:owner");
+$sta->bindParam(":owner", $ownerid);
+$sta->execute();
 
+$stb = $dbc->prepare("SELECT * FROM `product-keys` WHERE owner-id=:owner AND expire-date>:time");
+$stb->bindParam(":owner", $ownerid);
+$stb->bindParam(":time", $time);
+$stb->execute();
 
+if($sta->rowCount() == 0) {
+	$dbc = null;
+	die(er180);
+}
 
+// Grab the key from the database, hash with salt
+$row1 = $sta->fetch();
+$row2 = $stb->fetch();
 
+$key = sha1(sha1($row1['priv-key-a']).sha1($row2['key']).sha1($row1['priv-key-b']).sha1($row1['priv-key-c']));
 
+if($key !== sha1($productkey)) {
+	$dbc = null;
+	die(er998);
+} else {
+	$dbc = null;
+	die(er999);
+}
 
-
-
-
+$dbc = null;
 
 ?>
